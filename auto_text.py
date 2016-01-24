5289e095e901eacc5d73795d3169fe0883574b6d
shanna <shanna6633@gmail.com>
2016-01-23 23:18:01 -0800

    Auto_texter
    This is the first draft of auto_texter. The program is work with Twilio
    API.

---
+++ auto_text.py
@@ -0,0 +1,31 @@
+from twilio.rest import TwilioRestClient
+import time
+
+account = "AC169cd84f2b74f51bc8abebc42973cb6c"
+token = "54ea2786b8eb34dd3dd3a85696233f2d"
+client = TwilioRestClient(account, token)
+
+# # Phone numbers
+my_number  = '+15418339048'
+other_number = '+17189161886'
+body_1 = 'Thats play a game Jackie. Do something to stop count down.'
+end_body = 'BOOM!!!!!!!!!!!!!!!!!!!!!!!.'
+body_count = 'OMG! Dont let the bomb explore.'
+# body = 'http://wiesmann.codiferes.net/share/bitmaps/test_pattern.svg'
+
+# sent a SMS message
+for i in range(0, 7):
+    if i == 0:
+        message = client.messages.create(to=other_number, from_=my_number, body=body_1)
+        time.sleep(3)
+
+    elif i == 6:
+        time.sleep(6)
+        message = client.messages.create(to=other_number, from_=my_number, body=end_body)
+
+    else:
+        i = str(6 - i)
+        message = client.messages.create(to=other_number, from_=my_number, body=i + '\n' + body_count)
+        time.sleep(3)
+
+# message = client.messages.create(to=other_number, from_=my_number, body=body)
